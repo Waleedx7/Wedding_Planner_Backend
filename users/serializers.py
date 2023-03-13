029e0af9d5d0c626143ffde7da4472f996819516
mallollaa <mah41411@gmail.com>
2023-03-13 18:49:37 +0300

    test the User login

--- users/serializers.py
+++ users/serializers.py
@@ -1,36 +1,29 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+
 
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    access_token = serializers.SerializerMethodField()
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True) 
+   
     class Meta:
         model = User
-        fields = ['username', 'email', 'password','confirm_password']
-        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+        fields = ['username', 'password']
 
-    # def validate(self, data):
-    #     if data ['password'] != data.pop('confirm_password'):
-    #         raise serializers.ValidationError("Passwords must match")
-    #     return data
-    
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = User(**validated_data)
+        user = User(**validated_data) 
         user.set_password(password)
-        user.save()        
+        user.save()
         return user
-    
-   
-    
-class LoginSerializer(serializers.ModelSerializer):
+
+
+class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
-    # access_token = serializers.CharField(read_only=True)
+    access = serializers.CharField(read_only=True)
 
     def validate(self, data):
         username = data.get('username')
@@ -39,17 +32,11 @@ class LoginSerializer(serializers.ModelSerializer):
             user = User.objects.get(username=username)
             print(user.username)
         except User.DoesNotExist:
-            raise serializers.ValidationError("User not found")
+            print("This user doesn't exist")
+
         if not user.check_password(password):
-            raise serializers.ValidationError("Incorrect password")
+            raise serializers.ValidationError('Incorrect credentials')
+        
+        payload = RefreshToken.for_user(user)
+        data['access'] = str(payload.access_token)
         return data
-        
-    # def get_access_token(self,access_token):
-    #     payload = RefreshToken.for_user(access_token)
-    #     token = str(payload.access_token)
-    #     access_token['access_token'] = token
-    #     return access_token
-        
-        # access_token = RefreshToken.for_user(user)
-        # data['access_token'] = str(access_token.access_token)
-        # return data
