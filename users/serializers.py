729d11cb76c41fe3549c9b072505a69af44cb4da
mallollaa <mah41411@gmail.com>
2023-03-15 20:15:02 +0300

    the login and signin is working YAAYYY

--- users/serializers.py
+++ users/serializers.py
@@ -21,22 +21,23 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class LoginSerializer(serializers.Serializer):
+    
     username = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
-    email = serializers.CharField(read_only=True)
+   
     access = serializers.CharField(read_only=True)
 
     def validate(self, data):
         username = data.get('username')
         password = data.get('password')
-        # email = data.get('email')
+       
+       
         try:
             user = User.objects.get(username=username)
-            print(user.username)
+           
         except User.DoesNotExist:
             print("This user doesn't exist")
-        print(user)
-        print(password)
+      
         if not user.check_password(password):
             raise serializers.ValidationError('Incorrect credentials')
         
