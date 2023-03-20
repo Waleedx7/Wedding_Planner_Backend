7012e2b103240431a8168274bc76f7160c976d44
Waleedx7 <w.alshelahi@gmail.com>
2023-03-20 15:07:05 +0300

    some change in model and i added new serializer

--- vendors/urls.py
+++ vendors/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
+from .views import VendorsView , CategoryView
 
 urlpatterns = [
-    path('vendor/', views.vendor_list, name='vendor_list'),
+    path('list/', views.vendor_list, name='vendor_list'),
     path('category/', views.category_list, name='category_vendors'),
     path('create/', views.create_vendor, name='create_vendor'),
     path('create/category/', views.create_category, name='create_category'),
     path('update/<int:vendor_id>/', views.update_vendor, name='update_vendor'),
     path('delete/<int:vendor_id>/', views.delete_vendor, name='delete_vendor'),
+    path('api/vendors/', VendorsView.as_view(), name='VendorsList'),
+    path('api/categories/', CategoryView.as_view(), name='CategoryList'),
+    
 ]
